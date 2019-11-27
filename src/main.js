74fddef7661ee393243969f256373cf09bdf5ded
wuhu <wuhu@itcast.cn>
2019-11-27 11:59:46 +0800

    03-完成登陆组件的制作-还没有实现登陆的业务功能

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 
 // 引入路由模块
 import router from '@/router/index.js'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 引入全局的样式文件，包含了重置样式和字体样式以及项目中需要使用的公共样式
 import '@/styles/index.less'
+// 让Vue实例上能够挂载element-ui的所有组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router, // 注入路由
   render: h => h(App)
 }).$mount('#app')
